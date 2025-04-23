4fa855edaa1be36f0be829ab39b36040f66af601
Prakash Banjade <prakashbanjade191@gmail.com>
2025-04-24 01:03:08 +0545

    refactor: update start scripts and add ConfigService for dynamic port configuration

--- server/runner/src/main.ts
+++ server/runner/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { RunnerModule } from './runner.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(RunnerModule);
-  await app.listen(process.env.port ?? 3000);
+  
+  const configService = app.get(ConfigService);
+  
+  const PORT = configService.get("PORT") || 3000;
+  await app.listen(PORT);
+  console.log(`Api gateway listening on ${await app.getUrl()}`);
 }
 bootstrap();
